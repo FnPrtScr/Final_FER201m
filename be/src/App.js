1702686724cd64987f450557aaa85f955fdf2cdb
nguyenngoctuan <tn4490523@gmail.com>
2023-06-11 22:46:05 +0700

    init base be

---
+++ be/src/App.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+require('dotenv').config();
+const app = express();
+
+const { SERVER_PORT, DB_DATABASE, DB_HOST } = process.env;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+// app.use(
+//   '/api/v1',
+//   require('./routes')
+// );
+
+// if route not exist
+app.all('*', (req, res) => {
+  return res.status(404).json(errorResponse(404, 'Page Not Found...'));
+});
+
+app.listen(SERVER_PORT || 5000, async () => {
+  try {
+    console.log(`>>> Listening on port ${SERVER_PORT || 5000}`);
+
+    // await sequelize.authenticate();
+    // console.log(`>>> Connected to "${DB_DATABASE}" on "${DB_HOST}"!`);
+    // await sequelize.sync();
+    // console.log(`>>> Synced data successful`);
+
+  } catch (err) {
+    console.error(err);
+  }
+});
