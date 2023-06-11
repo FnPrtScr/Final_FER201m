0a68e8d6f9054e1a612c4ed4ebe1384681dfd7ef
nguyenngoctuan <tn4490523@gmail.com>
2023-06-11 23:15:51 +0700

    fix:  init base fe

--- fe/src/services/test.service.js
+++ fe/src/services/test.service.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import { Server } from "../dataConfig";
+// import { Server } from "../dataConfig";
 
 const UserService = {
   getUsers: async () => {
     try {
-      const response = await axios.get(`${Server}/users`);
+      // const response = await axios.get(`${Server}/users`);
+      const response = await axios.get(`https://647cb1eec0bae2880ad1148c.mockapi.io/users`);
       return response.data;
     } catch (error) {
       console.error('Error retrieving users:', error);
