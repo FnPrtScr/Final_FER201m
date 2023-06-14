0ab744a6bbc15e73d1019765bee535dfd6e993cf
nguyenngoctuan <tn4490523@gmail.com>
2023-06-14 21:34:36 +0700

    TestA

--- fe/src/App.js
+++ fe/src/App.js
@@ -3,7 +3,7 @@ import UserList from "./components/test.component";
 function App() {
   return (
     <div>
-      <h1>Welcome to Your React App</h1>
+      <h1>Welcome to TOO React App</h1>
       <UserList/>
     </div>
   );
