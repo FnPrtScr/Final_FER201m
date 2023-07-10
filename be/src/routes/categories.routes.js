d47a25e78170884fb38691382325873b2250b8be
FnPrtScr <tn4490523@gmail.com>
2023-07-10 23:39:14 +0700

    feat: schedule,fix: categories

--- be/src/routes/categories.routes.js
+++ be/src/routes/categories.routes.js
@@ -3,6 +3,7 @@ const CategoriesController = require('../controllers/categories.controller');
 
 router.get('/:id', CategoriesController.findCategory);
 router.get('/', CategoriesController.findCategories);
+router.get('/my-reminder/:id', CategoriesController.findAllByUserId);
 router.post('/', CategoriesController.createCategory);
 router.put('/:id', CategoriesController.updateCategory);
 router.delete('/:id', CategoriesController.deleteCategory);
