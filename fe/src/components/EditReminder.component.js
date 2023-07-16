bd17dbd276f4657da5878d2dedc19c5453b1278f
tuannden <tn4490523@gmail.com>
2023-07-16 23:43:37 +0700

    feat: edit reminder

--- fe/src/components/EditReminder.component.js
+++ fe/src/components/EditReminder.component.js
@@ -1,33 +1,19 @@
 import React from 'react'
 import { Col, Navbar, Row,Modal, Button, Form, FormLabel, Tab, NavItem, ListGroup, TabContainer } from 'react-bootstrap'
-
-const EditReminder = (props) => {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      style={{backgroundColor:'wheat'}}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  )
+import { useState,useParams, useEffect } from 'react';
+import DateTimePicker from 'react-datetime-picker'
+import { useNavigate } from 'react-router-dom'
+import { updateReminderById } from '../services/reminder.service';
+const EditReminder = () => {
+    const { reminder_id } = useParams();
+    console.log(reminder_id);
+    
+    // updateReminderById(reminder_id)
+    return (
+        <>
+           <div>abcd</div>
+        </>
+    );
 }
 
 export default EditReminder
